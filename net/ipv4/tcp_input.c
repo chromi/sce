df4f72504b1bd2838a3f13a64bdc0f5cefef0f59
Pete Heist <pete@heistp.net>
2019-05-02 22:06:29 +0200

    Implement an "every other" version of receiver feedback that alternates between ESCE marking and not when there's only one SCE marked packet out of two.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -383,31 +383,19 @@ static void __tcp_ecn_check_ce(struct sock *sk, const struct sk_buff *skb)
 		break;
 	case INET_ECN_SCE:
 		if (sock_net(sk)->ipv4.sysctl_tcp_sce) {
-			if (!(tp->ecn_flags & TCP_ECN_PRIOR_ESCE) &&
-				inet_csk(sk)->icsk_ack.pending & ICSK_ACK_TIMER) {
-				__tcp_send_ack(sk, tp->sce_prior_rcv_nxt);
+			if (tp->ecn_flags & TCP_ECN_MAYBE_ESCE) {
+				tp->ecn_flags |= TCP_ECN_QUEUE_ESCE;
+			} else {
+				tp->ecn_flags |= TCP_ECN_MAYBE_ESCE;
 			}
-			tcp_enter_quickack_mode(sk, 1);
-			tp->ecn_flags |= TCP_ECN_QUEUE_ESCE | TCP_ECN_PRIOR_ESCE | TCP_ECN_SEEN;
-			break;
 		}
-		/* fallthrough, since ECT(1) == ECT(0) in RFC-3168 */
-	default: /* INET_ECN_ECT */
-		if (sock_net(sk)->ipv4.sysctl_tcp_sce) {
-			if ((tp->ecn_flags & TCP_ECN_PRIOR_ESCE) &&
-				inet_csk(sk)->icsk_ack.pending & ICSK_ACK_TIMER) {
-				__tcp_send_ack(sk, tp->sce_prior_rcv_nxt);
-			}
-			tcp_enter_quickack_mode(sk, 1);
-			tp->ecn_flags &= ~(TCP_ECN_QUEUE_ESCE | TCP_ECN_PRIOR_ESCE);
-		}
+		break;
+	default:
 		if (tcp_ca_needs_ecn(sk))
 			tcp_ca_event(sk, CA_EVENT_ECN_NO_CE);
 		tp->ecn_flags |= TCP_ECN_SEEN;
 		break;
 	}
-
-	tp->sce_prior_rcv_nxt = tcp_sk(sk)->rcv_nxt;
 }
 
 static void tcp_ecn_check_ce(struct sock *sk, const struct sk_buff *skb)
