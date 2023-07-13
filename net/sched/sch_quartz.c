5bbdba5d62ddd2baa00ffed1c9170a46569c00bb
Pete Heist <pete@heistp.net>
2023-07-13 20:18:44 +0200

    Quartz: implement CoDel-like ramp adjuster

--- net/sched/sch_quartz.c
+++ net/sched/sch_quartz.c
@@ -5,20 +5,19 @@
  * Copyright (C) 2023 Pete Heist <pete@heistp.net>
  *
  * Quartz is an AQM that marks SCE in proportion to queue busy (non-empty)
- * time. The ramp is adjusted based on the specified busy time target,
- * converging on an operating point with low delay and high utilization. Quartz
- * pays no attention to the queue length, other than to enforce a hard limit.
+ * time, and adjusts the ramp to aim for the specified busy time target,
+ * hovering around an operating point with low delay and high utilization.
+ * Quartz pays no attention to the queue length, other than to enforce a hard
+ * limit.
  *
  * Parameters:
  *
- *   target     the target busy time before returning to idle
- *   busy_hold  the minimum allowed time between ramp increases on busy
- *   idle_hold  the minimum allowed time between ramp decreases on idle
- *   limit      hard limit, in packets
+ *   target    the target busy time before returning to idle
+ *   interval  the period between ramp updates
+ *   limit     hard limit, in packets
  *
  * TODO
  * - possibly return ramp to 32 bit
- * - check wisdom of resetting busy_held and idle_held in set_ramp
  * - optimize get_random call for size of ramp
  */
 
@@ -28,31 +27,32 @@
 
 /* AQM defaults (temporary until config code is in place) */
 #define DEFAULT_LIMIT 50
-#define DEFAULT_TARGET 5 * NSEC_PER_MSEC
-#define DEFAULT_IDLE_HOLD 10 * NSEC_PER_MSEC
-#define DEFAULT_BUSY_HOLD 100 * NSEC_PER_MSEC
+#define DEFAULT_TARGET 10 * NSEC_PER_MSEC
+#define DEFAULT_INTERVAL 200 * NSEC_PER_MSEC
 
 /* hard defines */
 #define RAMP_BASE 32
-#define CEILING_FACTOR 2
+
+/* interval_saw bitmask values */
+enum {
+	SAW_NONE		= 0,
+	SAW_OVER_TARGET		= BIT(0),
+	SAW_UNDER_TARGET	= BIT(1)
+};
 
 /* Debugging */
 //#define PRINT_BAR
 //#define PRINT_IDLE
-//#define PRINT_TARGET
 //#define PRINT_EVENTS
 
 struct quartz_params {
 	u32	target;
-	u32	idle_hold;
-	u32	busy_hold;
+	u32	interval;
 };
 
 struct quartz_sched_data {
 	struct quartz_params params;
 
-	u32	target_floor;
-	u32	target_ceil;
 	u8	ramp;
 	u8	ramp_shift;
 	u8	ramp_max;
@@ -60,12 +60,124 @@ struct quartz_sched_data {
 	u64	wall;
 	ktime_t	busy_start;
 	ktime_t	busy_prior;
-	ktime_t busy_held;
-	ktime_t idle_held;
-#ifdef PRINT_TARGET
-	u8	busy_hold_n;
-#endif
+	ktime_t	interval_end;
+	u8	interval_saw;
 };
+
+static void reset_interval(struct quartz_sched_data *q, ktime_t now)
+{
+	q->interval_end = ktime_add(now, q->params.interval);
+	q->interval_saw = SAW_NONE;
+}
+
+static void set_ramp(struct quartz_sched_data *q, u8 ramp, ktime_t now)
+{
+#ifdef PRINT_EVENTS
+	if (ramp != q->ramp)
+		printk("ramp -> %u\n", ramp);
+	if (ramp >= q->ramp_max)
+		printk("hit max ramp %u\n", ramp);
+#endif
+	q->ramp = ramp;
+	q->ramp_shift = RAMP_BASE + (ramp - 1);
+	q->wall = U64_MAX >> q->ramp_shift;
+}
+
+static void set_mark(struct quartz_sched_data *q, u64 mark)
+{
+/*
+#ifdef PRINT_EVENTS
+	static bool p;
+	if (!p) {
+		if (mark >= q->wall) {
+			printk("mark -> %llx\n", mark);
+			p = true;
+		}
+	} else {
+		p = (mark > 0);
+	}
+#endif
+*/
+	q->mark = mark;
+}
+
+static void increment_ramp(struct quartz_sched_data *q, ktime_t now)
+{
+	if (q->ramp < q->ramp_max) {
+		set_ramp(q, q->ramp + 1, now);
+		set_mark(q, q->mark >> 1);
+	}
+}
+
+static void decrement_ramp(struct quartz_sched_data *q, ktime_t now)
+{
+	if (q->ramp) {
+		set_ramp(q, q->ramp - 1, now);
+		set_mark(q, q->mark << 1);
+	}
+}
+
+static void on_interval_end(struct quartz_sched_data *q, ktime_t now)
+{
+	switch (q->interval_saw) {
+	case SAW_NONE:
+	case SAW_OVER_TARGET:
+		increment_ramp(q, now);
+		break;
+	case SAW_UNDER_TARGET:
+		decrement_ramp(q, now);
+		break;
+	}
+	reset_interval(q, now);
+}
+
+static void on_idle(struct quartz_sched_data *q, ktime_t now)
+{
+	struct quartz_params *p = &q->params;
+	s64 t = ktime_to_ns(ktime_sub(now, q->busy_start));
+
+	q->interval_saw |= (t > p->target) ? SAW_OVER_TARGET : SAW_UNDER_TARGET;
+
+	if (ktime_after(now, q->interval_end))
+		on_interval_end(q, now);
+
+	set_mark(q, 0);
+
+#ifdef PRINT_IDLE
+	printk("busy=%-16lldns delta=%lld\n", t, t - p->target);
+#endif
+}
+
+static void on_busy(struct quartz_sched_data *q, ktime_t now)
+{
+	if (ktime_after(now, q->interval_end))
+		on_interval_end(q, now);
+
+	if (q->ramp) {
+		u64 m = q->mark + ktime_to_ns(ktime_sub(now, q->busy_prior));
+		set_mark(q, m > q->wall ? q->wall : m);
+	}
+
+	q->busy_prior = now;
+}
+
+static u64 get_random_mark(struct quartz_sched_data *q)
+{
+	return get_random_u64() >> q->ramp_shift;
+}
+
+static void mark_random(struct quartz_sched_data *q, struct sk_buff *skb)
+{
+	if (!q->mark || !q->ramp)
+		return;
+
+	if (q->ramp < q->ramp_max) {
+		if (get_random_mark(q) < q->mark)
+			INET_ECN_set_ect1(skb);
+	} else {
+		INET_ECN_set_ect1(skb);
+	}
+}
 
 static void print_bar(int len)
 {
@@ -100,10 +212,8 @@ static s32 quartz_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 	if (!l0) {
 		q->busy_start = now;
 		q->busy_prior = now;
-		q->busy_held = ktime_add(now, ns_to_ktime(q->target_ceil));
-#ifdef PRINT_TARGET
-		q->busy_hold_n = 1;
-#endif
+		if (q->interval_saw == SAW_NONE)
+			reset_interval(q, now);
 	}
 
 	print_bar(qdisc_qlen(sch));
@@ -111,86 +221,10 @@ static s32 quartz_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 	return NET_XMIT_SUCCESS;
 }
 
-static void set_ramp(struct quartz_sched_data *q, u8 ramp, ktime_t now)
-{
-#ifdef PRINT_EVENTS
-	if (ramp != q->ramp)
-		printk("ramp -> %u\n", ramp);
-#endif
-	q->ramp = ramp;
-	q->ramp_shift = RAMP_BASE + (ramp - 1);
-	q->wall = U64_MAX >> q->ramp_shift;
-	/*
-	if (!q->ramp) {
-		q->busy_held = now;
-		q->idle_held = now;
-	}
-	*/
-}
-
-static void set_mark(struct quartz_sched_data *q, u64 mark)
-{
-#ifdef PRINT_EVENTS
-	if (q->mark < q->wall && mark >= q->wall)
-		printk("mark -> %llx\n", mark);
-#endif
-	q->mark = mark;
-}
-
-static void on_idle(struct quartz_sched_data *q, ktime_t now, s64 busy_ns)
-{
-	if (busy_ns < q->target_floor && ktime_after(now, q->idle_held)) {
-#ifdef PRINT_TARGET
-		s64 t = q->target_floor - busy_ns;
-		s64 h = ktime_to_ns(ktime_sub(now, q->idle_held));
-		printk("under target (-%lld ns, held +%lld ns)\n", t, h);
-#endif
-		if (q->ramp)
-			set_ramp(q, q->ramp - 1, now);
-
-		q->idle_held = ktime_add(now, ns_to_ktime(q->params.idle_hold));
-	}
-	set_mark(q, 0);
-#ifdef PRINT_IDLE
-	printk("idle busy_ns=%-16lld delta=%lld\n", busy_ns,
-		busy_ns - q->params.target);
-#endif
-}
-
-static void on_busy(struct quartz_sched_data *q, ktime_t now, s64 busy_ns)
-{
-	if (ktime_after(now, q->busy_held)) {
-#ifdef PRINT_TARGET
-		s64 t = busy_ns - q->params.target;
-		s64 h = ktime_to_ns(ktime_sub(now, q->busy_held));
-		printk("over target (n=%u, +%lld ns, held +%lld ns)\n",
-			q->busy_hold_n, t, h);
-		q->busy_hold_n++;
-#endif
-		if (q->ramp < q->ramp_max) {
-			set_ramp(q, q->ramp + 1, now);
-			set_mark(q, q->mark >> 1);
-		}
-		q->busy_held = ktime_add(now, ns_to_ktime(q->params.busy_hold));
-	}
-
-	if (q->ramp) {
-		u64 m = q->mark + ktime_to_ns(ktime_sub(now, q->busy_prior));
-		set_mark(q, m > q->wall ? q->wall : m);
-	}
-	q->busy_prior = now;
-}
-
-static u64 rand_mark(struct quartz_sched_data *q)
-{
-	return get_random_u64() >> q->ramp_shift;
-}
-
 static struct sk_buff* quartz_dequeue(struct Qdisc *sch)
 {
 	ktime_t now = ktime_get();
 	struct quartz_sched_data *q = qdisc_priv(sch);
-	s64 busy_ns = ktime_to_ns(ktime_sub(now, q->busy_start));
 	struct sk_buff *skb;
 
 	skb = qdisc_dequeue_head(sch);
@@ -200,13 +234,11 @@ static struct sk_buff* quartz_dequeue(struct Qdisc *sch)
 	}
 
 	if (!qdisc_qlen(sch))
-		on_idle(q, now, busy_ns);
+		on_idle(q, now);
 	else
-		on_busy(q, now, busy_ns);
+		on_busy(q, now);
 
-	if (q->mark && (rand_mark(q) < q->mark))
-		INET_ECN_set_ect1(skb);
-		//INET_ECN_set_ce(skb);
+	mark_random(q, skb);
 
 	print_bar(qdisc_qlen(sch));
 
@@ -220,11 +252,8 @@ static int quartz_init(struct Qdisc *sch, struct nlattr *opt,
 
 	memset(q, 0, sizeof(*q));
 	q->params.target = DEFAULT_TARGET;
-	q->params.idle_hold = DEFAULT_IDLE_HOLD;
-	q->params.busy_hold = DEFAULT_BUSY_HOLD;
-	q->target_floor = q->params.target / CEILING_FACTOR;
-	q->target_ceil = q->params.target * CEILING_FACTOR;
-	q->ramp_max = ilog2(q->params.target);
+	q->params.interval = DEFAULT_INTERVAL;
+	q->ramp_max = ilog2(q->params.target / 2);
 	set_ramp(q, 0, 0);
 
 	sch->limit = DEFAULT_LIMIT;
