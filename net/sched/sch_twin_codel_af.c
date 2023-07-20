24052b8f88cf72e84861199cfab22504366676b8
Pete Heist <pete@heistp.net>
2023-07-20 13:11:19 +0200

    sch_twin_codel_af: after rebase, call skb_protocol instead of tc_skb_protocol

--- net/sched/sch_twin_codel_af.c
+++ net/sched/sch_twin_codel_af.c
@@ -582,7 +582,7 @@ static u8 cnq_get_diffserv(struct sk_buff *skb)
 {
 	int wlen = skb_network_offset(skb);
 
-	switch (tc_skb_protocol(skb)) {
+	switch (skb_protocol(skb, true)) {
 	case htons(ETH_P_IP):
 		wlen += sizeof(struct iphdr);
 		if (!pskb_may_pull(skb, wlen))
