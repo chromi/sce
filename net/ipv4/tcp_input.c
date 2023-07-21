3573084b7493ece52d870e821884259007cac1d7
Pete Heist <pete@heistp.net>
2023-07-21 10:27:42 +0200

    tcp_input: propagate df561f6 to SCE code in tcp_input
    treewide: Use fallthrough pseudo-keyword

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -394,6 +394,7 @@ static void __tcp_ecn_check_ce(struct sock *sk, const struct sk_buff *skb)
 			break;
 		}
 		/* fallthrough - since ECT(1) == ECT(0) in RFC-3168 */
+		fallthrough;
 	default: /* INET_ECN_ECT */
 		if (sock_net(sk)->ipv4.sysctl_tcp_sce) {
 			if ((tp->ecn_flags & TCP_ECN_PRIOR_ESCE) &&
