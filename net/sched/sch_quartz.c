b3f7a740f2c399e2ec78ab5aa799034ae87df5a3
Pete Heist <pete@heistp.net>
2023-07-22 20:09:47 +0200

    Quartz: signal both SCE and CE

--- net/sched/sch_quartz.c
+++ net/sched/sch_quartz.c
@@ -5,11 +5,10 @@
  * Copyright (C) 2023 Pete Heist <pete@heistp.net>
  *
  * Quartz is an AQM that aims for low queueing delay with good utilization by
- * marking SCE such that the queue periodically and briefly returns to idle. It
- * does this by marking SCE in proportion to the queue sojourn time,
+ * marking SCE and CE such that the queue periodically and briefly returns to
+ * idle. It does this by marking in proportion to the queue sojourn time,
  * accumulated across the queue busy time. The marking ramp is modulated
- * according to a target busy time, thus automatically adapting to the
- * carried load.
+ * according to a target busy time, thus adapting to the carried load.
  *
  * Parameters:
  *
@@ -18,10 +17,6 @@
  *   floor          number of packets in queue at which it's considered busy
  *   sojourn_limit  delay time at which all packets are marked CE or dropped
  *   limit          hard limit, in packets
- *   split_gso      if true, split GSO aggregated packets
- *
- * TODO
- * - only calculate sojourn once
  */
 
 #include <net/pkt_sched.h>
@@ -29,18 +24,20 @@
 #include <net/inet_ecn.h>
 
 /* AQM defaults (temporary until config code is in place) */
-#define DEFAULT_TARGET (10 * NSEC_PER_MSEC)
+#define DEFAULT_TARGET (5 * NSEC_PER_MSEC)
 #define DEFAULT_INTERVAL (100 * NSEC_PER_MSEC)
 #define DEFAULT_FLOOR 0
 #define DEFAULT_SOJOURN_LIMIT (50 * NSEC_PER_MSEC)
 #define DEFAULT_LIMIT 1000
-#define DEFAULT_SPLIT_GSO false
 
 /* hard defines */
-#define RAMP_SHIFT 8 /* 8-10 for SCE, 26 for CE */
-#define MARK_MAX U64_MAX
+#define SHIFT_CE 2
+#define SHIFT_SCE 8
+#define MARK_MIN 0
+#define MARK_MAX (U64_MAX >> SHIFT_CE)
+#define MARK_MAX_SCE (U64_MAX >> SHIFT_SCE)
 #define RAMP_MIN 0
-#define RAMP_MAX (2 * (64 - RAMP_SHIFT))
+#define RAMP_MAX (2 * (64 - SHIFT_CE))
 #define RAMP_INFINITE U8_MAX
 
 #define SCE_DSCP_RTT_FAIR 3
@@ -58,35 +55,12 @@ struct quartz_params {
 	u32	interval;
 	u32	floor;
 	u32	sojourn_limit;
-	bool	split_gso;
 };
-
-struct marker {
-	u64	mark;
-	u8	ramp;
-	u8	ramp_min;
-	u8	ramp_max;
-	bool	ramp_zero;
-	bool	ramp_infinite;
-};
-
-// marker gets:
-// increment_ramp
-// decrement_ramp
-// mark_add(ktime_t busy, ktime_t sojourn)
-// random_mark
-// ktime_to_mark can be private
-// set_mark can be private, if it exists
-// set_ramp can be private, if it exists
 
 struct quartz_sched_data {
 	struct quartz_params params;
 
-	struct marker ce;
-	struct marker sce;
-
 	u8	ramp;
-	u8	ramp_max;
 	u64	mark;
 	ktime_t	busy_start;
 	ktime_t	busy_prior;
@@ -99,43 +73,9 @@ struct quartz_skb_cb {
 	ktime_t	enqueue_time;
 };
 
-typedef bool (*mark_func)(void);
-
-typedef s32 (*enqueue_func)(struct sk_buff *, struct Qdisc *, struct sk_buff **);
-
 /*
  * utility functions
  */
-
-static s32 split_gso(struct sk_buff *skb,
-		     struct Qdisc *sch,
-		     struct sk_buff **to_free,
-		     int len,
-		     enqueue_func enqueue)
-{
-	struct sk_buff *segs, *nskb;
-	netdev_features_t features = netif_skb_features(skb);
-	unsigned int slen = 0, numsegs = 0;
-
-	segs = skb_gso_segment(skb, features & ~NETIF_F_GSO_MASK);
-	if (IS_ERR_OR_NULL(segs))
-		return qdisc_drop(skb, sch, to_free);
-
-	skb_list_walk_safe(segs, segs, nskb) {
-		skb_mark_not_on_list(segs);
-
-		qdisc_skb_cb(segs)->pkt_len = segs->len;
-		slen += segs->len;
-		numsegs++;
-
-		enqueue(segs, sch, to_free);
-	}
-
-	qdisc_tree_reduce_backlog(sch, 1-numsegs, len - slen);
-	consume_skb(skb);
-
-	return NET_XMIT_SUCCESS;
-}
 
 static struct quartz_skb_cb *quartz_cb(const struct sk_buff *skb)
 {
@@ -143,19 +83,14 @@ static struct quartz_skb_cb *quartz_cb(const struct sk_buff *skb)
 	return (struct quartz_skb_cb *)qdisc_skb_cb(skb)->data;
 }
 
-static ktime_t enqueue_time(const struct sk_buff *skb)
-{
-	return quartz_cb(skb)->enqueue_time;
-}
-
 static void set_enqueue_time(struct sk_buff *skb, ktime_t t)
 {
 	quartz_cb(skb)->enqueue_time = t;
 }
 
-static ktime_t sojourn(struct sk_buff *skb, ktime_t now)
+static ktime_t sojourn_time(struct sk_buff *skb, ktime_t now)
 {
-	return ktime_sub(now, enqueue_time(skb));
+	return ktime_sub(now, quartz_cb(skb)->enqueue_time);
 }
 
 static u8 dscp(struct sk_buff *skb)
@@ -168,13 +103,11 @@ static u8 dscp(struct sk_buff *skb)
 		if (!pskb_may_pull(skb, l))
 			return 0;
 		return ipv4_get_dsfield(ip_hdr(skb)) >> 2;
-
 	case htons(ETH_P_IPV6):
 		l += sizeof(struct ipv6hdr);
 		if (!pskb_may_pull(skb, l))
 			return 0;
 		return ipv6_get_dsfield(ipv6_hdr(skb)) >> 2;
-
 	default:
 		return 0;
 	};
@@ -203,7 +136,7 @@ static void print_qlen_bar(int len)
 }
 
 /*
- * quartz_sched_data related functions
+ * quartz_sched_data functions
  */
 
 static u64 ktime_to_mark(struct quartz_sched_data *q, ktime_t t)
@@ -240,7 +173,7 @@ static void set_ramp(struct quartz_sched_data *q, u8 ramp)
 
 static void increment_ramp(struct quartz_sched_data *q)
 {
-	if (q->ramp < q->ramp_max)
+	if (q->ramp < RAMP_MAX)
 		set_ramp(q, q->ramp + 1);
 	else if (q->ramp != RAMP_INFINITE)
 		set_ramp(q, RAMP_INFINITE);
@@ -249,7 +182,7 @@ static void increment_ramp(struct quartz_sched_data *q)
 static void decrement_ramp(struct quartz_sched_data *q)
 {
 	if (q->ramp == RAMP_INFINITE)
-		set_ramp(q, q->ramp_max);
+		set_ramp(q, RAMP_MAX);
 	else if (q->ramp > RAMP_MIN)
 		set_ramp(q, q->ramp - 1);
 }
@@ -259,18 +192,22 @@ static bool sojourn_over_limit(struct quartz_sched_data *q,
 			       ktime_t now)
 {
 	u32 l = q->params.sojourn_limit;
-	return l && ktime_to_ns(sojourn(skb, now)) > l ? true : false;
-}
-
-static u64 random_mark(struct quartz_sched_data *q)
-{
-	return get_random_u64() >> RAMP_SHIFT;
+	return l && ktime_to_ns(sojourn_time(skb, now)) > l ? true : false;
 }
 
 static void mark_sce_random(struct quartz_sched_data *q, struct sk_buff *skb)
 {
-	if (q->ramp == RAMP_INFINITE || (q->mark && random_mark(q) < q->mark))
+	if (q->mark >= MARK_MAX_SCE ||
+	   (q->mark && (get_random_u64() >> SHIFT_SCE) < q->mark))
 		INET_ECN_set_ect1(skb);
+}
+
+static bool mark_ce_random(struct quartz_sched_data *q, struct sk_buff *skb)
+{
+	if (q->mark >= MARK_MAX ||
+	   (q->mark && (get_random_u64() >> SHIFT_CE) < q->mark))
+		return !INET_ECN_set_ce(skb);
+	return false;
 }
 
 static void reset_interval(struct quartz_sched_data *q, ktime_t now)
@@ -310,7 +247,7 @@ static void on_busy_dequeue(struct quartz_sched_data *q,
 		on_interval_end(q, now);
 
 	b = ktime_sub(now, q->busy_prior);
-	i = ktime_to_mark(q, b) * ktime_to_ns(sojourn(skb, now));
+	i = ktime_to_mark(q, b) * ktime_to_ns(sojourn_time(skb, now));
 	m = (i >= (MARK_MAX - q->mark) ? MARK_MAX : q->mark + i);
 	set_mark(q, m);
 
@@ -327,7 +264,7 @@ static void on_idle(struct quartz_sched_data *q, ktime_t now)
 	else
 		q->interval_under += (p->target - t);
 
-	set_mark(q, 0);
+	set_mark(q, MARK_MIN);
 
 #ifdef PRINT_IDLE
 	printk("busy=%-16lldns delta=%lld\n", t, t - p->target);
@@ -335,7 +272,7 @@ static void on_idle(struct quartz_sched_data *q, ktime_t now)
 }
 
 /*
- * qdisc implementation functions
+ * qdisc op functions
  */
 
 static s32 quartz_enqueue(struct sk_buff *skb,
@@ -347,9 +284,6 @@ static s32 quartz_enqueue(struct sk_buff *skb,
 	struct quartz_params *p = &q->params;
 	int len0 = qdisc_qlen(sch);
 	int r;
-
-	if (p->split_gso && skb_is_gso(skb))
-		return split_gso(skb, sch, to_free, len0, quartz_enqueue);
 
 	if (unlikely(sch->limit && len0 >= sch->limit)) {
 #ifdef PRINT_EVENTS
@@ -379,6 +313,7 @@ static struct sk_buff* quartz_dequeue(struct Qdisc *sch)
 	struct quartz_params *p = &q->params;
 	struct sk_buff *skb;
 	int len1;
+	bool drop = false;
 
 	skb = qdisc_dequeue_head(sch);
 	if (unlikely(!skb)) {
@@ -392,18 +327,23 @@ static struct sk_buff* quartz_dequeue(struct Qdisc *sch)
 
 	if (is_sce_dscp(dscp(skb)))
 		mark_sce_random(q, skb);
+	else
+		drop = mark_ce_random(q, skb);
 
 	if (len1 == p->floor)
 		on_idle(q, now);
 
-	print_qlen_bar(len1);
+	if (!drop && sojourn_over_limit(q, skb, now) && !INET_ECN_set_ce(skb))
+		drop = true;
 
-	if (sojourn_over_limit(q, skb, now) && !INET_ECN_set_ce(skb) && len1) {
+	if (drop && len1) {
 		qdisc_tree_reduce_backlog(sch, 1, qdisc_pkt_len(skb));
 		qdisc_qstats_drop(sch);
 		kfree_skb(skb);
 		return quartz_dequeue(sch);
 	}
+
+	print_qlen_bar(len1);
 
 	qdisc_bstats_update(sch, skb);
 
@@ -421,8 +361,6 @@ static int quartz_init(struct Qdisc *sch,
 	q->params.interval = DEFAULT_INTERVAL;
 	q->params.floor = DEFAULT_FLOOR;
 	q->params.sojourn_limit = DEFAULT_SOJOURN_LIMIT;
-	q->params.split_gso = DEFAULT_SPLIT_GSO;
-	q->ramp_max = RAMP_MAX;
 	set_ramp(q, RAMP_MIN);
 
 	sch->limit = DEFAULT_LIMIT;
